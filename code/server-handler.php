246b86bedb733fa226c70eca43b84deeab03a731
Sam Minnee <sam@silverstripe.com>
2016-09-12 16:30:57 +1200

    FIX: Pass directories to SilverStripe handler.
    This is needed otherwise admin/ and dev/ break. Matches regular
    SilverStripe semantics.

--- code/server-handler.php
+++ code/server-handler.php
@@ -21,7 +21,8 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-if ($uri !== "/" && file_exists(BASE_PATH . $uri)) {
+
+if ($uri !== "/" && file_exists(BASE_PATH . $uri) && !is_dir(BASE_PATH . $uri)) {
     return false;
 }
 
